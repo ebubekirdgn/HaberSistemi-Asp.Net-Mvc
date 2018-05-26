922b03c20b402aca11984f58c4f721b15ddee770
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-05-26 07:31:58 -0800

    Haber Created

---
+++ HaberSistemi/HaberSistemi.Data/Model/Haber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaberSistemi.Data.Model
+{
+    [Table("Haber")]
+    public class Haber
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Baslik { get; set; }
+        public string KisaAciklama { get; set; }
+        public string Aciklama { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+
+        public virtual Kullanici Kullanici { get; set; }
+        public string Resim { get; set; }
+    }
+}
