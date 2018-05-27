9927bf7673f8880185f3b669b19d08d8d4089886
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-05-27 00:40:18 -0800

    Method Created in IRepository Class

--- HaberSistemi/HaberSistemi.Core/Infrastructure/IRepostitory.cs
+++ HaberSistemi/HaberSistemi.Core/Infrastructure/IRepostitory.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HaberSistemi.Data.Infrastructure
 {
-    public interface IRepostitory
+    public interface IRepostitory<T> where T : class
     {
+        IEnumerable<T> GetAll(); // Tüm datamızı getirecek olan method
+
+        T GetById(int id); // Tek bir data getirecek olan method
+
+        T Get(Expression<Func<T, bool>> expression);
+
+        IQueryable<T> GetMany(Expression<Func<T, bool>> expression); //Birden fazla değer dönecek ama expressiona göre dönecek.
+
+        void Insert(T obj); // Ekleme işlemi yapacak olan method
+
+        void Update(T obj); //Güncelleme yapacak olan method
+
+        void Delete(int id); // Silme yapacak olan method
+
+        int Count();//Kayıt sayımızı verecek olan method
+
+        void Save();// Kaydetme methodumuz
     }
 }
