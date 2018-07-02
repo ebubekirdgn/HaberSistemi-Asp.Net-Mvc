619b2f59332c1e49f2082ffa9e7bea4accec6241
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-07-02 14:32:58 +0300

    güncel

--- HaberSistemi/HaberSistemi.Admin/Scripts/Global.js
+++ HaberSistemi/HaberSistemi.Admin/Scripts/Global.js
@@ -24,32 +24,30 @@
     });
 }
 
- 
-function KategoriDuzenle() {
-    Kategori = new Object();
-    Kategori.KategoriAdi = $("#KategoriAdi").val(); // Kategori Adı textindeki değer alınıyor.
-    Kategori.URL = $("#KategoriUrl").val(); // Kategori Url textindeki değer alınıyor.
-    Kategori.IsActive = $("#KategoriAktif").is(":checked");
-    Kategori.ParentID = $("#ParentID").val();
-    Kategori.ID = $("#ID").val();
-    $.ajax({
-        url: "/Kategori/Duzenle",
-        data: Kategori,
-        type: "POST",
-        datatype: 'json',
-        success: function (response) {
-            if (response.Success) {
-                bootbox.alert(response.Message, function () {
-                    location.reload();
-                });
-            }
-            else {
-                bootbox.alert(response.Message, function () {
-                    //TO DO :geri döndüğünde birşey yapılması isteniyorsa
-                });
-            }
+Kategori = new Object();
+Kategori.KategoriAdi = $("#KategoriAdi").val(); // Kategori Adı textindeki değer alınıyor.
+Kategori.URL = $("#KategoriUrl").val(); // Kategori Url textindeki değer alınıyor.
+Kategori.IsActive = $("#KategoriAktif").is(":checked");
+Kategori.ParentID = $("#ParentID").val();
+Kategori.ID = $("#ID").val();
+$.ajax({
+    url: "/Kategori/Duzenle",
+    data: Kategori,
+    type: "POST",
+    datatype: 'json',
+    success: function (response) {
+        if (response.Success) {
+            bootbox.alert(response.Message, function () {
+                location.reload();
+            });
         }
-    });
+        else {
+            bootbox.alert(response.Message, function () {
+                //TO DO :geri döndüğünde birşey yapılması isteniyorsa
+            });
+        }
+    }
+});
 }
 
 $(document).on("click", "#KategoriDelete", function () {
